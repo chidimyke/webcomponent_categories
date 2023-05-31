2a6cfcc32cee6a4877c5546c3ce15994e24edf7e
chidimyke <ejimonuchidiebere@gmail.com>
2023-05-31 23:47:44 +0100

    fetch with Async-Await

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ const doubleBtn = document.getElementById("double");
 const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// console.log(data);
+// fetch action: random user and add money
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  // console.log(data);
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  // console.log(newUser);
+  // A created function
+  addData(newUser);
+}
+
+// Function definition for addData()
+// This function adds a new item to the array per function call
+
+function addData(obj) {
+  data.push(obj);
+}
