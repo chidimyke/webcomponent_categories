d266d0ac90d16735a16ec8acde8fa75add479496
chidimyke <ejimonuchidiebere@gmail.com>
2023-06-02 08:52:58 +0100

    foreach method

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
 
+// array definition
 let data = [];
 
-getRandomUser();
-getRandomUser();
-getRandomUser();
+// getRandomUser();
+// getRandomUser();
+// getRandomUser();
 
 // console.log(data);
 // fetch action: random user and add money
@@ -32,18 +33,35 @@ async function getRandomUser() {
 }
 
 // Function definition for addData()
-// This function adds a new item to the array per function call
-
 function addData(obj) {
+  // This function adds a new item to the array per function call
+  // it is a part of the getRandomUser() above
   data.push(obj);
   updateDOM();
 }
 
 // Update Dom Function
 function updateDOM(providedData = data) {
+  // This function updates the DOM based on the fetch parameters; it is a part of the addData() above
   // clear main div
 
   main.innerHTML = `<h2><strong>Person</strong>Wealth</h2>`;
 
-  providedData.forEach();
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong>${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
 }
+
+// money format function
+function formatMoney(number) {
+  // This returns a value formatted to look like a currency number based on the regular expression
+  return "$" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+}
+
+// Event Listeners
+addUserBtn.addEventListener("click", getRandomUser);
